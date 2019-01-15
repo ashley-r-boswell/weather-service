8c7a3a7e9388e2ecce5af44c2f6f842e633fed21
ashley <ashley.r.boswell@gmail.com>
2019-01-15 23:12:28 +0100

    Add test for city controller. Only for success scenarios so far.

---
+++ test/controllers/CityController.spec.js
@@ -0,0 +1,56 @@
+import CityController from '../../src/controllers/CityController'
+import CityInformationService from '../../src/services/CityInformationService'
+import {
+  CITY_VIEW_WITH_COORDINATES,
+  CITY_ID_AND_NAME
+} from '../fixtures/Cities'
+
+jest.mock('../../src/services/CityInformationService')
+
+let mockRequest
+let mockResponse
+let mockNextCallback
+let serviceMock
+
+beforeEach(() => {
+  mockRequest = {}
+  mockResponse = { send: jest.fn(result => {}) }
+  mockNextCallback = jest.fn(() => {})
+  serviceMock = new CityInformationService()
+})
+
+describe('get city', () => {
+  test('by id', () => {
+    serviceMock.getCity.mockImplementation(cityId => CITY_VIEW_WITH_COORDINATES)
+    const cityController = new CityController(serviceMock)
+    mockRequest.params = { city_id: '5' }
+
+    cityController.getCity(mockRequest, mockResponse, mockNextCallback)
+
+    expect(serviceMock.getCity).toHaveBeenCalledTimes(1)
+    expect(serviceMock.getCity).toHaveBeenCalledWith(5)
+    expect(mockResponse.send).toHaveBeenCalledTimes(1)
+    expect(mockResponse.send).toHaveBeenCalledWith(CITY_VIEW_WITH_COORDINATES)
+    expect(mockNextCallback).toHaveBeenCalledTimes(1)
+    expect(mockNextCallback).toHaveBeenCalledWith()
+  })
+})
+
+describe('find nearby cities', () => {
+  test('by latitude and longitude', () => {
+    serviceMock.findNearbyCities.mockImplementation((latitude, longitude) => [
+      CITY_ID_AND_NAME
+    ])
+    const cityController = new CityController(serviceMock)
+    mockRequest.query = { lat: '21', lng: '22' }
+
+    cityController.findNearbyCities(mockRequest, mockResponse, mockNextCallback)
+
+    expect(serviceMock.findNearbyCities).toHaveBeenCalledTimes(1)
+    expect(serviceMock.findNearbyCities).toHaveBeenCalledWith(21, 22)
+    expect(mockResponse.send).toHaveBeenCalledTimes(1)
+    expect(mockResponse.send).toHaveBeenCalledWith([CITY_ID_AND_NAME])
+    expect(mockNextCallback).toHaveBeenCalledTimes(1)
+    expect(mockNextCallback).toHaveBeenCalledWith()
+  })
+})
