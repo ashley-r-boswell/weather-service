10a511657e560ccee588a5490f6500e76a6a6c70
ashley <ashley.r.boswell@gmail.com>
2019-01-15 21:19:27 +0100

    Document the server steps by extracting parts into function.

--- src/WeatherServer.js
+++ src/WeatherServer.js
@@ -3,24 +3,34 @@ import WeatherController from './controllers/WeatherController'
 
 class WeatherServer {
   async start() {
-    const server = restify.createServer({
+    this.buildServer()
+    this.configureRoutes()
+    return this.listen()
+  }
+
+  buildServer() {
+    this.server = restify.createServer({
       name: 'weather-service',
       version: '1.0.0'
     })
 
-    server.use(restify.plugins.acceptParser(server.acceptable))
-    server.use(restify.plugins.queryParser())
-    server.use(restify.plugins.bodyParser())
+    this.server.use(restify.plugins.acceptParser(this.server.acceptable))
+    this.server.use(restify.plugins.queryParser())
+    this.server.use(restify.plugins.bodyParser())
+  }
 
-    server.get('/echo/:name', (req, res, next) => {
+  configureRoutes() {
+    this.server.get('/echo/:name', (req, res, next) => {
       res.send(req.params)
       return next()
     })
-    server.get('/cities/:city_id/weather', (req, res, next) =>
+    this.server.get('/cities/:city_id/weather', (req, res, next) =>
       WeatherController.getWeatherForCity(req, res, next)
     )
+  }
 
-    return new Promise(resolve => server.listen(8080, resolve))
+  async listen() {
+    return new Promise(resolve => this.server.listen(8080, resolve))
   }
 }
 
