c1cf7986ad4be91a380748a1e4893e04fec2d491
ashley <ashley.r.boswell@gmail.com>
2019-01-15 18:09:16 +0100

    Add a service function which looks for cities within 10km.

--- src/services/CityInformationService.js
+++ src/services/CityInformationService.js
@@ -1,6 +1,8 @@
 import CompressedJsonFileLoader from '../clients/CompressedJsonFileLoader'
 import _ from 'lodash'
+import haversine from 'haversine'
 
+const NEARBY_RADIUS = 10000
 export default class CityInformationService {
   constructor(fileName) {
     this.fileName = fileName
@@ -13,5 +15,16 @@ export default class CityInformationService {
 
   getCity(cityId) {
     return _.find(this.cities, city => city.id === cityId)
+  }
+
+  findNearbyCities(latitude, longitude) {
+    return _.filter(this.cities, city => {
+      const metersToCity = haversine(
+        { latitude: latitude, longitude: longitude },
+        { latitude: city.coord.lat, longitude: city.coord.lon },
+        { unit: 'meter' }
+      )
+      return metersToCity <= NEARBY_RADIUS
+    })
   }
 }
--- test/services/CityInformationService.spec.js
+++ test/services/CityInformationService.spec.js
@@ -12,3 +12,27 @@ test('get city by id - correct', async () => {
 
   expect(result).toEqual(CITY)
 })
+
+test('find nearby cities - finds exact match', async () => {
+  const service = new CityInformationService(
+    './test/resources/tiny.city.list.json.gz'
+  )
+
+  await service.init()
+
+  const nearbyCities = service.findNearbyCities(28, 85.416664)
+
+  expect(nearbyCities).toContainEqual(CITY)
+})
+
+test('find nearby cities - finds nearby match', async () => {
+  const service = new CityInformationService(
+    './test/resources/tiny.city.list.json.gz'
+  )
+
+  await service.init()
+
+  const nearbyCities = service.findNearbyCities(28.085, 85.42)
+
+  expect(nearbyCities).toContainEqual(CITY)
+})
