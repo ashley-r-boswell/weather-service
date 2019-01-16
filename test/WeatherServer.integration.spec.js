be3275b5cb85ed6a20b8a135c9cf09b1a4c15e3e
ashley <ashley.r.boswell@gmail.com>
2019-01-16 12:03:19 +0100

    Add integration test for finding nearby cities.

--- test/WeatherServer.integration.spec.js
+++ test/WeatherServer.integration.spec.js
@@ -1,7 +1,7 @@
 import WeatherServer from '../src/WeatherServer'
 import path from 'path'
 import request from 'request'
-import { CITY_VIEW_WITH_COORDINATES } from './fixtures/Cities'
+import { CITY_VIEW_WITH_COORDINATES, CITY_ID_AND_NAME } from './fixtures/Cities'
 import { WEATHER_INFORMATION } from './fixtures/Weather'
 import nock from 'nock'
 import { SUCCESSFUL_RESPONSE } from './fixtures/OpenWeatherMap'
@@ -9,7 +9,7 @@ import { SUCCESSFUL_RESPONSE } from './fixtures/OpenWeatherMap'
 let testConfig
 
 describe('weather server integration test', () => {
-  beforeEach(() => {
+  beforeEach(async () => {
     testConfig = {
       openweathermap: {
         apiKey: 'myAppId'
@@ -28,24 +28,40 @@ describe('weather server integration test', () => {
       .filteringPath(/appid=.*/g, 'appid=myAppId')
       .get('/data/2.5/weather?id=1283710&appid=myAppId')
       .reply(200, SUCCESSFUL_RESPONSE)
+
+    await WeatherServer.start(testConfig)
+  })
+
+  afterEach(async () => {
+    await WeatherServer.stop()
   })
 
   test('get city by id', async done => {
-    await WeatherServer.start(testConfig)
-
     expect.assertions(3)
     request('http://localhost:8080/cities/1283710', (error, response, body) => {
       expect(error).toBeFalsy()
       expect(response.statusCode).toEqual(200)
       expect(JSON.parse(body)).toEqual(CITY_VIEW_WITH_COORDINATES)
 
-      WeatherServer.stop().then(done)
+      done()
     })
   })
 
-  test('get weather by city id', async done => {
-    await WeatherServer.start(testConfig)
+  test('get nearby cities', async done => {
+    expect.assertions(3)
+    request(
+      'http://localhost:8080/cities?lat=28&lng=85.416664',
+      (error, response, body) => {
+        expect(error).toBeFalsy()
+        expect(response.statusCode).toEqual(200)
+        expect(JSON.parse(body)).toContainEqual(CITY_ID_AND_NAME)
 
+        done()
+      }
+    )
+  })
+
+  test('get weather by city id', async done => {
     expect.assertions(3)
     request(
       'http://localhost:8080/cities/1283710/weather',
@@ -54,7 +70,7 @@ describe('weather server integration test', () => {
         expect(response.statusCode).toEqual(200)
         expect(JSON.parse(body)).toEqual(WEATHER_INFORMATION)
 
-        WeatherServer.stop().then(done)
+        done()
       }
     )
   })
